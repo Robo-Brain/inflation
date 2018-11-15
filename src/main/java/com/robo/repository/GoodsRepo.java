251136b5cccae1192f8d702755b4ad11650cc31d
Robo-Brain <svechkov.p@gmail.com>
2018-11-15 22:24:22 +0500

    Update GoodsRepo.java

--- src/main/java/com/robo/repository/GoodsRepo.java
+++ src/main/java/com/robo/repository/GoodsRepo.java
@@ -2,6 +2,7 @@ package com.robo.repository;
 
 import com.robo.Entities.Goods;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
