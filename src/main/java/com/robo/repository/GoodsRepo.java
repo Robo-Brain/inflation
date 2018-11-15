4e2097187fbfa158ca325d5dc273a3618d321e56
Robo-Brain <svechkov.p@gmail.com>
2018-11-15 22:08:28 +0500

    Update GoodsRepo.java

--- src/main/java/com/robo/repository/GoodsRepo.java
+++ src/main/java/com/robo/repository/GoodsRepo.java
@@ -13,7 +13,7 @@ public interface GoodsRepo extends JpaRepository<Goods, Integer> {
     @Query("SELECT g FROM Goods g ORDER BY g.name ASC")
     List<Goods> findAllOrderByNameAsc();
 
-   List<Goods> findByNameContainingIgnoreCase(String letter);
+   List<Goods> findByNameContainingIgnoreCase("к");
 
 //     @Query("SELECT g FROM Goods AS g WHERE UPPER(g.name) LIKE CONCAT('%',UPPER('К'),'%')")
 //     List<Goods> findByNameStartingWith(@Param("letter") String letter);
