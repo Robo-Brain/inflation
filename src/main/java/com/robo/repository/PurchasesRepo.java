4a0aa5362dc4fb94359f60e085f01ff2cd09bcc8
Robo-Brain <svechkov.p@gmail.com>
2018-11-15 21:31:11 +0500

    Update PurchasesRepo.java

--- src/main/java/com/robo/repository/PurchasesRepo.java
+++ src/main/java/com/robo/repository/PurchasesRepo.java
@@ -2,8 +2,6 @@ package com.robo.repository;
 
 import com.robo.Entities.Purchases;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.Optional;
@@ -11,10 +9,5 @@ import java.util.Optional;
 public interface PurchasesRepo extends JpaRepository<Purchases, Integer> {
 
     Optional<Purchases> findByUserIdAndShopIdAndProductIdAndDate (String userId, Integer shopId, Integer productId, LocalDate date);
-        
-//     Optional<Purchases> findByShopIdStartingWith(Integer num);
-    
-   @Query("SELECT p FROM Purchases p WHERE p.shopId LIKE :num ||'%'")
-   Optional<Purchases> findByShopIdStartingWith(@Param("num") Integer num);
 
 }
