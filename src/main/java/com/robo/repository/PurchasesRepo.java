ebf93a280c1a751606b16f4922e52741a73705ed
Robo-Brain <svechkov.p@gmail.com>
2018-11-15 21:29:03 +0500

    Update PurchasesRepo.java

--- src/main/java/com/robo/repository/PurchasesRepo.java
+++ src/main/java/com/robo/repository/PurchasesRepo.java
@@ -2,6 +2,8 @@ package com.robo.repository;
 
 import com.robo.Entities.Purchases;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.Optional;
