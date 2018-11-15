4b461064db63cadf9d15224051cda7996cb1debe
Robo-Brain <svechkov.p@gmail.com>
2018-11-15 21:43:42 +0500

    Update GoodsRepo.java

--- src/main/java/com/robo/repository/GoodsRepo.java
+++ src/main/java/com/robo/repository/GoodsRepo.java
@@ -17,7 +17,7 @@ public interface GoodsRepo extends JpaRepository<Goods, Integer> {
 
 //    List<Goods> findByNameStartingWith(String letter);
 
-    @Query("SELECT g FROM Goods AS g WHERE g.name LIKE :letter%")
+    @Query("SELECT g FROM Goods AS g WHERE UPPER(g.name) LIKE CONCAT('%',UPPER(:letter),'%')")
     List<Goods> findByNameStartingWith(@Param("letter") String letter);
 
 }
