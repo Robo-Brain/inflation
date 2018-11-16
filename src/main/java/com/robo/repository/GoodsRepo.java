68827170276da62bb6ef35ce252705bb54c2893f
Robobrain <svechkov.p@gmail.com>
2018-11-16 10:44:51 +0500

    strange bug, version to check fix

--- src/main/java/com/robo/repository/GoodsRepo.java
+++ src/main/java/com/robo/repository/GoodsRepo.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-public interface GoodsRepo extends JpaRepository<Goods, Integer> {
+public interface GoodsRepo extends JpaRepository<Goods, String> {
 
     List<Goods> findByName(String name);
 
